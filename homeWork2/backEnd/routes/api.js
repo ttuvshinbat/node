b259288303f66e9f83f791fb55d33ddf885cf9cf
tuvshinbat t <tuvshinbat@gmail.com>
2022-02-09 19:06:55 +0800

    added

--- homeWork2/backEnd/routes/api.js
+++ homeWork2/backEnd/routes/api.js
@@ -4,6 +4,7 @@ const path = require("path")
 const app = express()
 const books = require("../book.json")
 
+let nom = books.books
 let a = [];
 function randomIntFromInterval(min, max, count) {
     var random = Math.floor(Math.random() * (max - min + 1) + min)
@@ -28,6 +29,32 @@ app.get("/", (req, res) => {
 app.get("/books", (req, res) => {
     res.render("books", { nom: books.books })
 })
+app.get("/author", (req, res) => {
+    res.render("author", { nom: books.books })
+})
+app.get("/book/:isbn_id", (req, res) => {
+    let isbn = req.params.isbn_id
+    nom.filter(data => {
+        if (isbn == data.isbn) {
+            res.send(data)
+        }
+    })
+})
+app.get("/maxpages", (req, res) => {
+    let max = Math.max(...nom.map(({ pages }) => pages))
+    let result = nom.filter(({ pages }) => pages === max)
+    res.send(result)
+})
+app.get("/minpages", (req, res) => {
+    let min = Math.min(...nom.map(({ pages }) => pages))
+    let result = nom.filter(({ pages }) => pages === min)
+    res.send(result)
+})
+
+app.get("/add", (req, res) => {
+    res.render("add", { nom: books.books })
+
+})
 console.log(rndInt)
-console.log(a)
+
 module.exports = app;
