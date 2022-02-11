3a007bcce16598e1a4786b906bb5a3b76cc5f0fc
tuvshinbat t <tuvshinbat@gmail.com>
2022-02-11 12:36:40 +0800

    /add validator bolohgui baigaag medej baigaa

--- homeWork2/backEnd/routes/api.js
+++ homeWork2/backEnd/routes/api.js
@@ -29,9 +29,12 @@ app.set("view option", { layout: false })
 app.get("/", (req, res) => {
     res.render("index", { nom: books.books, rndInt: rndInt, })
 })
-app.get("/books", (req, res) => {
-    res.render("books", { nom: books.books })
-})
+app.get("/books",
+
+    (req, res) => {
+        res.render("books", { nom: books.books })
+
+    })
 app.get("/author", (req, res) => {
     res.render("author", { nom: books.books })
 })
@@ -43,8 +46,8 @@ app.get("/book/:isbn_id", (req, res) => {
         }
     })
 })
-app.post("/add", body("isbn").isNumeric().isLength({ min: 12, max: 13 }),
-    body("title").notEmpty(),
+app.use("/add", body("isbn").isNumeric().isLength({ min: 12, max: 13 }),
+    body("title").isEmpty(),
     body("subtitle").notEmpty(),
     body("author").notEmpty(),
     body("published").notEmpty(),
@@ -54,7 +57,12 @@ app.post("/add", body("isbn").isNumeric().isLength({ min: 12, max: 13 }),
     body("website").notEmpty(),
 
     (req, res) => {
+        const errors = validationResult(req)
         console.log(req.body)
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() })
+        } else (res.send("data"))
+
         const data = req.body
         date = new Date(Date.now())
         fs.appendFile("book_add.json",
@@ -62,7 +70,7 @@ app.post("/add", body("isbn").isNumeric().isLength({ min: 12, max: 13 }),
             () => { }
         )
 
-        res.send("bolq bn oo")
+
     })
 app.get("/maxpages", (req, res) => {
     let max = Math.max(...nom.map(({ pages }) => pages))
