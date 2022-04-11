53a1fe52b02c345291c3bbf75a550d497b986538
tuvshinbat t <tuvshinbat@gmail.com>
2022-04-11 20:35:24 +0800

    cors nemev

--- mongo/index.js
+++ mongo/index.js
@@ -5,8 +5,19 @@ const app = express();
 const apiRoutes = require("./routes/api");
 const adminRoutes = require("./routes/admin");
 const connection = require("./database");
+const cors = require("cors");
+
 app.use(express.json());
 app.use(express.static("public"));
+app.use(cors());
+app.post("/", function requestHandle(req, res) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Content-Type", "application/json");
+});
 app.use("/api", apiRoutes);
 app.all("*", (req, res, next) => {
   res.status(404).json({
